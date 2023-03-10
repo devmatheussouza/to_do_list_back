6a006b1e28cd3b5e9b635683009646d58f67c972
devmatheussouza <matheus-oliveira17@hotmail.com>
2023-03-10 18:41:54 -0300

    [Add] Enabling cors for tests.

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,15 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(function (_, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.get("/", (_, res) => {
   res.send("To do list App!");
 });
